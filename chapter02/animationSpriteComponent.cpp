c8fc8472a75591a019bf9c3f143a1f29095ca9df
AconCavy <aconcavy@gmail.com>
2021-03-20 06:30:19 +0900

    Update animationSpriteComponent

--- chapter02/animationSpriteComponent.cpp
+++ chapter02/animationSpriteComponent.cpp
@@ -3,6 +3,12 @@
 
 namespace gpcpp::c02 {
 
+AnimationSpriteComponent::AnimationSpriteComponent(class Actor *owner, int drawOrder)
+	: SpriteComponent(owner, drawOrder),
+	  _currentFrame(0),
+	  _animationFps(24) {
+}
+
 void AnimationSpriteComponent::Update(float deltaTime) {
   SpriteComponent::Update(deltaTime);
 
@@ -12,4 +18,13 @@ void AnimationSpriteComponent::Update(float deltaTime) {
 	SetTexture(_animationTextures[static_cast<int>(_currentFrame)]);
   }
 }
+
+void AnimationSpriteComponent::SetAnimationTextures(const std::vector<SDL_Texture *> &textures) {
+  _animationTextures = textures;
+  if (!_animationTextures.empty()) {
+	_currentFrame = 0;
+	SetTexture(_animationTextures[0]);
+  }
+}
+
 } // namespace gpcpp::c02
