1700405ac2d15c8c6313e14223fec2f1bda78138
AconCavy <aconcavy@gmail.com>
2021-03-20 06:38:03 +0900

    Update backgroundSpriteComponent

--- chapter02/backgroundSpriteComponent.cpp
+++ chapter02/backgroundSpriteComponent.cpp
@@ -1,6 +1,12 @@
 #include "backgroundSpriteComponent.hpp"
+#include "actor.hpp"
 
 namespace gpcpp::c02 {
+
+BackgroundSpriteComponent::BackgroundSpriteComponent(class Actor *owner, int drawOrder)
+	: SpriteComponent(owner, drawOrder),
+	  _scrollSpeed(0) {
+}
 
 void BackgroundSpriteComponent::Update(float deltaTime) {
   SpriteComponent::Update(deltaTime);
@@ -8,6 +14,19 @@ void BackgroundSpriteComponent::Update(float deltaTime) {
 	background._offset.x += _scrollSpeed * deltaTime;
 	if (background._offset.x < -_screenSize.x)
 	  background._offset.x = (_textures.size() - 1) * _screenSize.x - 1;
+  }
+}
+
+void BackgroundSpriteComponent::Draw(SDL_Renderer *renderer) {
+  SpriteComponent::Draw(renderer);
+  for (auto &background : _textures) {
+	SDL_Rect r;
+	r.w = static_cast<int>(_screenSize.x);
+	r.h = static_cast<int>(_screenSize.y);
+	r.x = static_cast<int>(_owner->GetPosition().x - r.w / 2 + background._offset.x);
+	r.x = static_cast<int>(_owner->GetPosition().y - r.h / 2 + background._offset.y);
+
+	SDL_RenderCopy(renderer, background._texture, nullptr, &r);
   }
 }
 
