dd29bb193dee1492bf78c2f1741579c429c189d9
AconCavy <aconcavy@gmail.com>
2021-03-22 06:10:31 +0900

    Update math to add getRandomRange

--- utils/math.hpp
+++ utils/math.hpp
@@ -14,6 +14,20 @@ static inline float isNearZero(float Value) {
   return abs(Value) < EPS;
 }
 
+static inline long getRandomIntRange(long Min, long Max) {
+  std::random_device Rnd;
+  std::mt19937 MT32(Rnd());
+  std::uniform_int_distribution<long> Range(Min, Max);
+  return Range(MT32);
+}
+
+static inline double getRandomRealRange(double Min, double Max) {
+  std::random_device Rnd;
+  std::mt19937 MT32(Rnd());
+  std::uniform_real_distribution<double> Range(Min, Max);
+  return Range(MT32);
+}
+
 } // namespace gpcpp
 
 #endif //GPCPP_UTILS_MATH_HPP
