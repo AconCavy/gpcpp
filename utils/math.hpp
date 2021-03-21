e47ff868f44986d666caf42b2c7d38ab073f8781
AconCavy <aconcavy@gmail.com>
2021-03-22 03:13:50 +0900

    Modify c++ version

--- utils/math.hpp
+++ utils/math.hpp
@@ -1,12 +1,12 @@
 #ifndef GPCPP_UTILS_MATH_HPP
 #define GPCPP_UTILS_MATH_HPP
 
-#include "numbers"
-
 namespace gpcpp {
 
+const double PI = 3.14159;
+
 static inline float toDegree(float Radian) {
-  return static_cast<float>(Radian * 180.0f / std::numbers::pi);
+  return static_cast<float>(Radian * 180.0f / PI);
 }
 
 } // namespace gpcpp
