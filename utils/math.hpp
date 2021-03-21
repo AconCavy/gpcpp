c75ef0b5785b5d975b788c24a8245239bd1f838e
AconCavy <aconcavy@gmail.com>
2021-03-22 05:39:47 +0900

    Update math to add isNearZero

--- utils/math.hpp
+++ utils/math.hpp
@@ -4,9 +4,14 @@
 namespace gpcpp {
 
 const double PI = 3.14159;
+const double EPS = 1e-6;
 
 static inline float toDegree(float Radian) {
   return static_cast<float>(Radian * 180.0f / PI);
+}
+
+static inline float isNearZero(float Value) {
+  return abs(Value) < EPS;
 }
 
 } // namespace gpcpp
