8b84be940f19d6e7f9cedf88387cac36869de265
AconCavy <aconcavy@gmail.com>
2021-03-23 23:39:18 +0900

    Add ship

---
+++ chapter03/ship.cpp
@@ -0,0 +1,27 @@
+#include "ship.hpp"
+
+#include "game.hpp"
+#include "inputComponent.hpp"
+#include "math.hpp"
+
+using namespace gpcpp::c03;
+
+Ship::Ship(class Game *Game)
+	: Actor(Game) {
+  auto SC = new SpriteComponent(this, 150);
+  SC->setTexture(Game->getTexture("assets/Ship.png"));
+
+  auto IC = new InputComponent(this);
+  IC->setForwardKey(SDL_SCANCODE_W);
+  IC->setBackKey(SDL_SCANCODE_S);
+  IC->setClockwiseKey(SDL_SCANCODE_A);
+  IC->setCounterClockwiseKey(SDL_SCANCODE_D);
+  IC->setMaxAngularSpeed(static_cast<float>(gpcpp::PI * 2));
+  IC->setMaxForwardSpeed(300);
+}
+
+void Ship::updateActor(float DeltaTime) {
+}
+
+void Ship::ActorInput(const uint8_t *KeyState) {
+}
---
+++ chapter03/ship.hpp
@@ -0,0 +1,17 @@
+#ifndef GPCPP_CHAPTER02_SHIP_HPP
+#define GPCPP_CHAPTER02_SHIP_HPP
+
+#include "actor.hpp"
+
+namespace gpcpp::c03 {
+
+class Ship : public Actor {
+public:
+  explicit Ship(class Game *Game);
+  void updateActor(float DeltaTime) override;
+  void ActorInput(const uint8_t *KeyState) override;
+};
+
+} // namespace gpcpp::c03
+
+#endif //GPCPP_CHAPTER02_SHIP_HPP
