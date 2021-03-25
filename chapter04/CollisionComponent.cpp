b66183da4c73ee10310da05e4305794106132f72
AconCavy <aconcavy@gmail.com>
2021-03-26 06:35:31 +0900

    Copy CollisionComponent

---
+++ chapter04/CollisionComponent.cpp
@@ -0,0 +1,26 @@
+#include "CollisionComponent.hpp"
+
+#include "Actor.hpp"
+
+using namespace gpcpp::c04;
+
+CollisionComponent::CollisionComponent(class Actor *Owner)
+    : Component(Owner), Radius(0), Center({}) {}
+
+const glm::vec2 &CollisionComponent::getCenter() const {
+  return Owner->getPosition();
+}
+float CollisionComponent::getRadius() const {
+  return Owner->getScale() * Radius;
+}
+
+bool CollisionComponent::isColliding(const CollisionComponent &other) const {
+  if (!IsEnabled)
+    return false;
+
+  auto Diff = getCenter() - other.getCenter();
+  auto D2 = Diff.x * Diff.x + Diff.y * Diff.y;
+  auto R = getRadius() + other.getRadius();
+
+  return D2 <= R * R;
+}
---
+++ chapter04/CollisionComponent.hpp
@@ -0,0 +1,28 @@
+#ifndef GPCPP_CHAPTER03_COLLISIONCOMPONENT_HPP
+#define GPCPP_CHAPTER03_COLLISIONCOMPONENT_HPP
+
+#include <glm/glm.hpp>
+
+#include "Component.hpp"
+
+namespace gpcpp::c04 {
+
+class CollisionComponent : public Component {
+public:
+  explicit CollisionComponent(Actor *Owner);
+
+  [[nodiscard]] bool isColliding(const CollisionComponent &other) const;
+
+  [[nodiscard]] const glm::vec2 &getCenter() const;
+  [[nodiscard]] float getRadius() const;
+
+  void setRadius(float R) { Radius = R; }
+
+private:
+  float Radius;
+  glm::vec2 Center;
+};
+
+} // namespace gpcpp::c04
+
+#endif // GPCPP_CHAPTER03_COLLISIONCOMPONENT_HPP
